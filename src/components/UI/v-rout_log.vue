2eba8e0f4e4b217bde56fb9d36b90273a8bf5da9
KiwiNick1984 <kiwi181284@gmail.com>
2022-09-21 16:32:46 +0300

    style scoped

--- src/components/UI/v-rout_log.vue
+++ src/components/UI/v-rout_log.vue
@@ -31,7 +31,7 @@ export default {
 };
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 .rout-log {
   position: relative;
   display: flex;
--- src/components/UI/v-sensor_log.vue
+++ src/components/UI/v-sensor_log.vue
@@ -30,7 +30,7 @@ export default {
 };
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 .sensor-log {
   display: flex;
   justify-content: space-between;
